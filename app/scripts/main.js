1dfc392e8888f4f33bee94e60fc7ce9b7664e662
INOUE Takuya <inouetakuya5@gmail.com>
2018-10-07 23:24:25 +0900

    Failed to subscribe the user: DOMException: Registration failed - permission denied

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -61,10 +61,22 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 }
 
 function initializeUI () {
+  pushButton.addEventListener('click', function () {
+    pushButton.disabled = true;
+
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser();
+    }
+  });
+
   // Set the initial subscription value
   swRegistration.pushManager.getSubscription()
     .then(function (subscription) {
       isSubscribed = !(subscription === null);
+
+      updateSubscriptionOnServer(subscription);
 
       if (isSubscribed) {
         console.log('User IS subscribed.');
@@ -84,4 +96,40 @@ function updateBtn () {
   }
 
   pushButton.disabled = false;
+}
+
+function subscribeUser () {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+    .then(function (subscription) {
+      console.log('User is subscribed:', subscription);
+
+      updateSubscriptionOnServer(subscription);
+
+      isSubscribed = true;
+
+      updateBtn();
+    })
+    .catch(function (err) {
+      console.log('Failed to subscribe the user: ', err);
+      updateBtn();
+    });
+}
+
+function updateSubscriptionOnServer (subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails = document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
 }
