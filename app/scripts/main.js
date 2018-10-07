8d9a6e4840793e926c625e4d207128dae93a44e3
INOUE Takuya <inouetakuya5@gmail.com>
2018-10-07 18:32:47 +0900

    Service Worker is registered

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,7 +28,7 @@ const pushButton = document.querySelector('.js-push-btn');
 let isSubscribed = false;
 let swRegistration = null;
 
-function urlB64ToUint8Array(base64String) {
+function urlB64ToUint8Array (base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
     .replace(/\-/g, '+')
@@ -41,4 +41,20 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+    .then(function (swReg) {
+      console.log('Service Worker is registered', swReg);
+      swRegistration = swReg;
+    })
+    .catch(function (error) {
+      console.error('Service Worker Error', error);
+    });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
 }
